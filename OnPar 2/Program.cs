d8226ac6ffccb4ab73a6af22b9f061b7db4cb66d
Ben Demko <bendemko7@gmail.com>
2016-07-21 20:50:39 -0400

    setup

--- OnPar 2/Program.cs
+++ OnPar 2/Program.cs
@@ -10,6 +10,58 @@ namespace OnPar_2
     {
         static void Main(string[] args)
         {
+
+            string again;
+            do
+
+            {
+
+                Console.WriteLine("Enter first number: ");
+                string number1AsString = Console.ReadLine();
+                int number1 = Convert.ToInt32(number1AsString);
+
+                Console.WriteLine("Enter operation: ");
+                string calcAsString = Console.ReadLine();
+                char calc = Convert.ToChar(calcAsString);
+
+                Console.WriteLine("Enter second number:");
+                string number2AsString = Console.ReadLine();
+                int number2 = Convert.ToInt32(number2AsString);
+
+
+
+
+                if (calc == '+')
+
+                    Console.WriteLine(number1 + number2);
+
+
+                else if (calc == '-')
+
+                    Console.WriteLine(+number1 - number2);
+
+                else if (calc == '*')
+
+                    Console.WriteLine(number1 * number2);
+
+                else if (calc == '/')
+
+                    Console.WriteLine(number1 / number2);
+
+                Console.WriteLine("Would you like to perform another calculation, yes or no?");
+                again = Console.ReadLine();
+
+            }
+
+
+            while (again == "YES");
+
+            Console.WriteLine("Goodbye");
+
+            Console.ReadKey();
+
+
+
         }
     }
 }
